9fac39d417ea649d1449949f5e39fde112640ec9
D-G-Dimitrov <5708006+D-G-Dimitrov@users.noreply.github.com>
2025-09-11 08:16:40 +0300

    set llama-cpp-python source path

--- packages/llm/llama_cpp/build.sh
+++ packages/llm/llama_cpp/build.sh
@@ -7,7 +7,7 @@ INSTALL_CPP=${SOURCE_DIR}/build/dist
 set -ex
 
 git clone --recursive --branch=${LLAMA_CPP_BRANCH_PY} https://github.com/abetlen/llama-cpp-python ${SOURCE_DIR} || \
-git clone --recursive https://github.com/abetlen/llama-cpp-python
+git clone --recursive https://github.com/abetlen/llama-cpp-python ${SOURCE_DIR}
 
 if [ -n "${LLAMA_CPP_BRANCH}" ]; then
     cd ${SOURCE_CPP}
