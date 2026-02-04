eec6fcce0c0d4af9658a31cee6b22426f7385efd
D-G-Dimitrov <5708006+D-G-Dimitrov@users.noreply.github.com>
2026-02-04 21:16:43 +0200

    Update build

--- packages/llm/llama_cpp/build.sh
+++ packages/llm/llama_cpp/build.sh
@@ -1,34 +1,23 @@
 #!/usr/bin/env bash
 echo "Building llama-cpp-python ${LLAMA_CPP_VERSION_PY}"
 
-SOURCE_CPP=${SOURCE_DIR}/vendor/llama.cpp
+SOURCE_CPP=/opt/llama.cpp
 INSTALL_CPP=${SOURCE_DIR}/build/dist
 
 set -ex
 
-git clone --recursive --branch=${LLAMA_CPP_BRANCH_PY} https://github.com/abetlen/llama-cpp-python ${SOURCE_DIR} || \
-git clone --recursive https://github.com/abetlen/llama-cpp-python
-
-if [ -n "${LLAMA_CPP_BRANCH}" ]; then
-    cd ${SOURCE_CPP}
-    git checkout ${LLAMA_CPP_BRANCH}
-fi
-
-cd ${SOURCE_DIR}
-
-FORCE_CMAKE=1 \
-CMAKE_ARGS="${LLAMA_CPP_FLAGS} -DLLAVA_BUILD=OFF -DCMAKE_CUDA_ARCHITECTURES=${CUDA_ARCHITECTURES}" \
-uv build --wheel --out-dir ${PIP_WHEEL_DIR} --verbose .
-
-uv pip install ${PIP_WHEEL_DIR}/llama_cpp_python*.whl
-uv pip show llama-cpp-python
-
-twine upload --verbose ${PIP_WHEEL_DIR}/llama_cpp_python*.whl || echo "failed to upload wheel to ${TWINE_REPOSITORY_URL}"
+git clone --recursive --branch=${LLAMA_CPP_VERSION} --depth=1 https://github.com/${LLAMA_CPP_REPO} ${SOURCE_DIR}
 
 # install c++ binaries
 cd ${SOURCE_CPP}
 
-cmake -B build ${LLAMA_CPP_FLAGS} \
+apt update && apt install -y ccache libssl-dev
+
+cmake -B build \
+    -DGGML_CUDA=on \
+    -DGGML_CUDA_F16=on \
+    -DLLAMA_CURL=on \
+    -DGGML_CUDA_FA_ALL_QUANTS=ON \
     -DCMAKE_CUDA_ARCHITECTURES=${CUDA_ARCHITECTURES} \
     -DCMAKE_INSTALL_PREFIX=${INSTALL_CPP} \
     -DLLAMA_BUILD_SERVER=ON \
@@ -39,10 +28,6 @@ cmake -B build ${LLAMA_CPP_FLAGS} \
 cmake --build build --config Release --parallel $(nproc)
 cmake --install build
 
-# upload packages to apt server
-tarpack upload llama-cpp-${LLAMA_CPP_VERSION} ${INSTALL_CPP} || echo "failed to upload tarball"
 echo "installed" > "$TMP/.llama_cpp"
-cp -r ${INSTALL_CPP}/* /usr/local/
 
-# create link to /opt/llama.cpp
-ln -s ${SOURCE_DIR}/vendor/llama.cpp /opt/llama.cpp
+cp -r ${INSTALL_CPP}/* /usr/local/
--- packages/llm/llama_cpp/config.py
+++ packages/llm/llama_cpp/config.py
@@ -1,30 +1,45 @@
-GGUF_FLAGS="-DGGML_CUDA=on -DGGML_CUDA_F16=on -DLLAMA_CURL=on -DGGML_CUDA_FA_ALL_QUANTS=ON"
-GGML_FLAGS="-DLLAMA_CUBLAS=on -DLLAMA_CUDA_F16=1"
 from jetson_containers import CUDA_ARCHITECTURES
 
-def llama_cpp(version, default=False, flags=GGUF_FLAGS):
+def get_latest_release_tag(repo_path):
+    """Get the latest release tag from a GitHub repository.
+
+    Args:
+        repo_path: GitHub repo path like user/repo
+
+    Returns:
+        str: Latest release tag name
     """
-    Define container that builds both llama.cpp and llama-cpp-python.
-    Different versions may have some different flag options activated.
-    """
-    cpp = bool(version[0] == 'b')
+
+    # Call GitHub API
+    api_url = f"https://api.github.com/repos/{repo_path}/releases/latest"
+    import requests
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        return response.json()['tag_name']
+    else:
+        raise Exception(
+            f"Failed to fetch release from {api_url} : {response.status_code} - {response.text}")
+
+def llama_cpp(version,  repo=None, default=False):
+
+    if repo and version == 'stable':
+        version = get_latest_release_tag(repo)
+        # strip the starting v if exists
+        if version.startswith('v'):
+            version = version[1:]
+
     pkg = package.copy()
 
     pkg['name'] = f'llama_cpp:{version}'
 
     pkg['build_args'] = {
-        'LLAMA_CPP_VERSION': version[1:] if cpp else None,
-        'LLAMA_CPP_VERSION_PY': '0.3.16' if cpp else version,
-        'LLAMA_CPP_BRANCH': version if cpp else None,
-        'LLAMA_CPP_BRANCH_PY': 'main' if cpp else f'v{version}',
-        'LLAMA_CPP_FLAGS': flags,
+        'LLAMA_CPP_REPO': repo,
+        'LLAMA_CPP_VERSION': version,
         'CUDA_ARCHITECTURES': ';'.join([str(x) for x in CUDA_ARCHITECTURES]),
     }
 
-    if cpp:
-        test_model = "bartowski/Qwen_Qwen3-1.7B-GGUF/Qwen_Qwen3-1.7B-Q4_K_M.gguf"
-    else:
-        test_model = "TheBloke/Llama-2-7B-GGUF/llama-2-7b.Q4_K_S.gguf"
+    test_model = "bartowski/Qwen_Qwen3-1.7B-GGUF/Qwen_Qwen3-1.7B-Q4_K_M.gguf"
 
     pkg['test'] = pkg['test'] + [
         f"test_model.py --model $(huggingface-downloader {test_model})"
@@ -41,21 +56,5 @@ def llama_cpp(version, default=False, flags=GGUF_FLAGS):
     return pkg, builder
 
 package = [
-    llama_cpp('0.2.57', flags=GGML_FLAGS),
-    llama_cpp('0.2.70', flags=GGML_FLAGS),
-    llama_cpp('0.2.83'),
-    llama_cpp('0.2.90'),
-    llama_cpp('0.3.1'),
-    llama_cpp('0.3.2'),
-    llama_cpp('0.3.5'),
-    llama_cpp('0.3.6'),
-    llama_cpp('0.3.7'),
-    llama_cpp('0.3.8'),
-    llama_cpp('0.3.9'),
-    llama_cpp('0.4.0'),
-
-    # llama_cpp_python appears abandoned (4/25)
-    # so we changed over to llama.cpp branches
-    llama_cpp('b5255'),
-    llama_cpp('b7481', default=True)
+    llama_cpp('stable', repo='ggml-org/llama.cpp', default=True)
 ]
