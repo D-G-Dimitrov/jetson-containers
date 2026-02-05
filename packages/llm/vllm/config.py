604598dabe5406a140a9fdc961beefc0fca4da47
D-G-Dimitrov <5708006+D-G-Dimitrov@users.noreply.github.com>
2026-02-05 11:25:58 +0200

    vllm v0.15.1

--- packages/llm/vllm/config.py
+++ packages/llm/vllm/config.py
@@ -37,6 +37,6 @@ package = [
     vllm('0.12.0', depends=['flashinfer:0.5.4'], default=False),
     vllm('0.13.0', depends=['flashinfer:0.5.4'], default=False),
     vllm('0.14.0', depends=['flashinfer:0.5.4'], default=True),
-    vllm('0.15.0', depends=['flashinfer'], default=True),
+    vllm('0.15.1', depends=['flashinfer'], default=True),
     vllm('latest', 'main', depends=['flashinfer'], default=False),
 ]
