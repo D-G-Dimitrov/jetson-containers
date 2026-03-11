96b5f1d68162fbb3649710e544603be5c2632bfe
D-G-Dimitrov <5708006+D-G-Dimitrov@users.noreply.github.com>
2026-03-11 17:08:06 +0200

    add fa-4 install

--- packages/attention/flash-attention/install.sh
+++ packages/attention/flash-attention/install.sh
@@ -6,5 +6,5 @@ if [ "$FORCE_BUILD" == "on" ]; then
 	exit 1
 fi
 
-uv pip install flash-attn==${FLASH_ATTENTION_VERSION}
+uv pip install -v flash-attn==${FLASH_ATTENTION_VERSION} flash-attn-4
 uv pip show flash-attn && python3 -c 'import flash_attn'
