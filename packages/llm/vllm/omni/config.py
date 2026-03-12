1fa61bc093c06ca1a85d9a0fcbfa8de25f6088a0
D-G-Dimitrov <5708006+D-G-Dimitrov@users.noreply.github.com>
2026-03-12 14:49:17 +0200

    add omni v0.17.0

--- packages/llm/vllm/omni/config.py
+++ packages/llm/vllm/omni/config.py
@@ -33,5 +33,6 @@ def vllm_omni(version, branch, repo=None, depends=None, requires=None, default=F
 
 package = [
     vllm_omni('0.14.0', 'release/v0.14.0',  depends=['vllm'] , repo='vllm-project/vllm-omni',default=False),
-    vllm_omni('0.16.0', 'release/v0.16.0' ,depends=['vllm:0.16.0'] , repo='vllm-project/vllm-omni',default=True)
+    vllm_omni('0.16.0', 'release/v0.16.0' ,depends=['vllm:0.16.0'] , repo='vllm-project/vllm-omni',default=False),
+    vllm_omni('0.17.0', 'v0.17.0rc1' ,depends=['vllm:0.17.0'] , repo='vllm-project/vllm-omni',default=True)
 ]
