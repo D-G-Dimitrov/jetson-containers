b3e225531669df1f6ab3a6af8f99eecf7a16d660
D-G-Dimitrov <5708006+D-G-Dimitrov@users.noreply.github.com>
2026-04-15 20:22:30 +0300

    vLLM install script update

--- packages/llm/vllm/install.sh
+++ packages/llm/vllm/install.sh
@@ -22,7 +22,8 @@ if [ "$FORCE_BUILD" == "on" ]; then
 	exit 1
 fi
 
-uv pip install -U vllm==${VLLM_VERSION}
+uv pip uninstall vllm
+uv pip install vllm==${VLLM_VERSION}
 uv pip install ray[cgraph] fastsafetensors "transformers>5"
 uv pip install -U mistral_common
 
