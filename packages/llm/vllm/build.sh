57d4b9a1140e059fc9c1a8257e1205e1c3d77400
D-G-Dimitrov <5708006+D-G-Dimitrov@users.noreply.github.com>
2026-06-01 15:13:31 +0300

    revert changes

--- packages/llm/vllm/build.sh
+++ packages/llm/vllm/build.sh
@@ -12,7 +12,6 @@ set -ex
 
 uv pip install pre-commit nanobind==2.5.0 flashinfer-python==0.6.12 flashinfer-cubin==0.6.12 flashinfer-jit-cache==0.6.12
 
-rm -rf /opt/vllm
 # Clone the repository if it doesn't exist
 git clone --branch=${VLLM_BRANCH} --recursive --depth=1 https://github.com/vllm-project/vllm /opt/vllm ||
 git clone --recursive --depth=1 https://github.com/vllm-project/vllm /opt/vllm
@@ -72,7 +71,7 @@ uv build --wheel --no-build-isolation -v --out-dir /opt/vllm/wheels .
 # Optionally upload to a repository using Twine
 twine upload --verbose /opt/vllm/wheels/vllm*.whl || echo "Failed to upload wheel to ${TWINE_REPOSITORY_URL}"
 
-uv pip install /opt/vllm/wheels/vllm*.whl #--index-url https://pypi.org/simple
+uv pip install /opt/vllm/wheels/vllm*.whl --index-url https://pypi.org/simple
 
 cd /opt/vllm
 uv pip install compressed-tensors
