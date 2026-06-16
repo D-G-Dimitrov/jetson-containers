2536781defe6905fe90799bcb0f3cc57d32465a4
D-G-Dimitrov <5708006+D-G-Dimitrov@users.noreply.github.com>
2026-06-16 13:57:45 +0300

    re-introduce version for install

--- packages/ml/mamba/causalconv1d/install.sh
+++ packages/ml/mamba/causalconv1d/install.sh
@@ -6,5 +6,5 @@ if [ "$FORCE_BUILD" == "on" ]; then
 	exit 1
 fi
 
-uv pip install causal_conv1d || \
-uv pip install causal_conv1d
+uv pip install causal_conv1d==${CASUALCONV1D_VERSION} || \
+uv pip install causal_conv1d==${CASUALCONV1D_VERSION_SPEC}
